e83da35ae9b2cee8b923a6c9c799c3c25aa258e2
Hampa Hug <hampa@hampa.ch>
2021-08-13 14:37:33 +0200

    rc759: Assert IRQ when there is a valid key available
    This prevents the keyboard from getting stuck with
    a valid key available but with IRQ deasserted.

--- src/arch/rc759/keyboard.c
+++ src/arch/rc759/keyboard.c
@@ -5,7 +5,7 @@
 /*****************************************************************************
  * File name:   src/arch/rc759/keyboard.c                                    *
  * Created:     2012-07-01 by Hampa Hug <hampa@hampa.ch>                     *
- * Copyright:   (C) 2012-2013 Hampa Hug <hampa@hampa.ch>                     *
+ * Copyright:   (C) 2012-2021 Hampa Hug <hampa@hampa.ch>                     *
  *****************************************************************************/
 
 /*****************************************************************************
@@ -423,6 +423,7 @@ void rc759_kbd_clock (rc759_kbd_t *kbd, unsigned cnt)
 	}
 
 	if (kbd->key_valid) {
+		rc759_kbd_set_irq (kbd, 1);
 		return;
 	}
 
@@ -432,7 +433,6 @@ void rc759_kbd_clock (rc759_kbd_t *kbd, unsigned cnt)
 	kbd->key_valid = 1;
 
 	kbd->key_i = (kbd->key_i + 1) % RC759_KBD_BUF;
-
 
 	rc759_kbd_check_mouse (kbd);
 
