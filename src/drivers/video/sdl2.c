7d35a1db34b45b6126417bac1382b9a81eb2600a
joshua stein <jcs@jcs.org>
2022-05-04 10:53:14 -0500

    sdl2: Put some chatty debug messages behind #ifdef DEBUG_SDL2

--- src/drivers/video/sdl2.c
+++ src/drivers/video/sdl2.c
@@ -601,7 +601,9 @@ void sdl2_event_window (sdl2_t *sdl, SDL_WindowEvent *evt)
 		break;
 
 	default:
+#ifdef DEBUG_SDL2
 		fprintf (stderr, "sdl2: window event %u\n", evt->event);
+#endif
 		break;
 	}
 }
@@ -650,7 +652,9 @@ void sdl2_check (sdl2_t *sdl)
 			break;
 
 		default:
+#ifdef DEBUG_SDL2
 			fprintf (stderr, "sdl2: event %u\n", evt.type);
+#endif
 			break;
 		}
 	}
